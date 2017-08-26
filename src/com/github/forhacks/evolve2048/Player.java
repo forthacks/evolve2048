0ea489f6c4d241f5cea1e4ac32fbdcde232a35c2
thegithubgeek <wfrankw9@gmail.com>
2017-08-26 13:46:09 -0700

    Change Player

--- src/com/github/forhacks/evolve2048/Player.java
+++ src/com/github/forhacks/evolve2048/Player.java
@@ -10,10 +10,32 @@ public class Player {
     static int MIN_INIT_LAYER=2;
     static int MAX_INIT_NODES = 10;
     static int MIN_INIT_NODES = 4;
+    static double REMOVE_PROB = 0.05;
+    static double ADD_PROB = 0.05;
+    static double LAYER_ADD_PROB = 0.001;
+    Player(ArrayList<Layer> l){
+        layers = l;
+    }
+    void mutate(){
+        for (int i=0; i<layers.size(); i++) {
+            layers.get(i).mutate((i>0)?layers.get(i-1).nodes.size():16);
+        }
+        if(chance(LAYER_ADD_PROB)){
+            layers.add(Layer.ranLayer(layers.get(layers.size()-1).nodes.size()));
+        }
+    }
+    static Player ranPlayer(){
+        int layerNum = (int) (Math.random()*(MAX_INIT_LAYER-MIN_INIT_LAYER)+MIN_INIT_LAYER);
+        Player p = new Player(new ArrayList<>());
+        for(int i=0; i<layerNum; i++) {
+            p.layers.add(Layer.ranLayer((i>0)?p.layers.get(i-1).nodes.size():16));
+        }
+        return p;
+    }
     int run(int[][] grid){
         Layer start = new Layer(new ArrayList<Node>());
-        for (int[] arr:grid) {
-            for (int value:arr){
+        for (int[] arr : grid) {
+            for (int value : arr){
                 start.nodes.add(new Node(value));
             }
         }
@@ -21,14 +43,14 @@ public class Player {
         layers.add(start);
         for(int i=1; i<layers.size(); i++) {
             layers.get(i).reset();
-            if(i%2==0){
+            if(i%2 == 0){
                 layers.get(i).max(layers.get(i-1));
             }else{
                 layers.get(i).and(layers.get(i-1));
             }
         }
         int[] newMovement = new int[4];
-        for (Node n:layers.get(layers.size()-1).nodes) {
+        for (Node n : layers.get(layers.size()-1).nodes) {
             for(int i=0; i<4; i++) {
                 newMovement[i] += n.movement[i];
             }
@@ -76,6 +98,22 @@ public class Player {
                 nodes.set(toNode, Node.max(nodes.get(n1), nodes.get(n2)));
             }
         }
+        void mutate(int prevLayerSize){
+            for(int i=0; i<connections.size(); i++){
+                if(chance(REMOVE_PROB)){
+                    connections.remove(i);
+                    i--;
+                }
+            }
+            if(chance(ADD_PROB)){
+                int n1 = (int)(Math.random()*prevLayerSize);
+                int n2 = (int)(Math.random()*prevLayerSize);
+                while(n2==n1){
+                    n2 = (int)(Math.random()*prevLayerSize);
+                }
+                connections.add(new Connection(n1, n2, (int)(Math.random()*nodes.size())));
+            }
+        }
         static Layer ranLayer(int prevLayerSize){
             Layer l = new Layer(new ArrayList<>(),new ArrayList<>());
             int nodeNum = (int) (Math.random()*(MAX_INIT_NODES-MIN_INIT_NODES)+MIN_INIT_NODES);
@@ -91,6 +129,9 @@ public class Player {
             return l;
         }
     }
+    static boolean chance(Double d){
+        return Math.random()<d;
+    }
     static class Connection{
         int n1, n2, toNode;
         Connection(int n1, int n2, int toNode){
@@ -101,17 +142,17 @@ public class Player {
     }
     static class Node {
         int value;
-        float[] movement; // FORMAT: [up, left, down, right]
+        double[] movement; // FORMAT: [up, left, down, right]
         Node(int value){
             this.value = value;
-            movement = new float[4];
+            movement = new double[4];
         }
-        Node(int value, float[] movement){
+        Node(int value, double[] movement){
             this.value = value;
             this.movement = movement;
         }
         static Node and(Node n1, Node n2){
-            float[] newMovement = new float[4];
+            double[] newMovement = new double[4];
             if(n1.value != n2.value) {
                 return new Node(0, newMovement);
             }
@@ -126,7 +167,7 @@ public class Player {
             } else if (n1.value < n2.value) {
                 return n2;
             }
-            float[] newMovement = new float[4];
+            double[] newMovement = new double[4];
             for (int i = 0; i < 4; i++) {
                 newMovement[i] = (n1.movement[i]+n2.movement[i])/2;
             }
