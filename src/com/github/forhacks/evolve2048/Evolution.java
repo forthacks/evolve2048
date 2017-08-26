68860f8842007b315e4c91d4ba1d0e7fd1c6ecdc
Oliver Ni <oliver.ni@gmail.com>
2017-08-26 10:56:08 -0700

    Add Evolution, Game, and Player classes
    Evolution - Algorithm
    Game - Actual 2048 Game
    Player - Plays Game with Evolution

---
+++ src/com/github/forhacks/evolve2048/Evolution.java
@@ -0,0 +1,9 @@
+package com.github.forhacks.evolve2048;
+
+/**
+ * Created by mcparadip on 8/26/17.
+ */
+
+public class Evolution {
+
+}
---
+++ src/com/github/forhacks/evolve2048/Game.java
@@ -0,0 +1,8 @@
+package com.github.forhacks.evolve2048;
+
+/**
+ * Created by mcparadip on 8/26/17.
+ */
+
+public class Game {
+}
