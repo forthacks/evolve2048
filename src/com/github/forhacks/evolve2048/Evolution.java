f1675eddfb47a0570a4cf885f23daa838ddf504c
kongriley <kongriley@gmail.com>
2017-08-26 19:09:30 -0700

    Add ripple-block search

--- src/com/github/forhacks/evolve2048/Evolution.java
+++ src/com/github/forhacks/evolve2048/Evolution.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 
 public class Evolution {
 
-    private static final int PLAYER_NUM = 6;
+    private static final int PLAYER_NUM = 15;
     private static final int NUM_TRIAL = 5;
     private static final int GEN_NUM = 10;
     private static final double KILL_RATE = 0.5;
@@ -25,15 +25,24 @@ public class Evolution {
             Arrays.fill(scores, 0);
             for (int j = 0; j < PLAYER_NUM; j++) {
                 score = 0;
+                int count = 0;
                 for (int k = 0; k < NUM_TRIAL; k++) {
                     Main.g.initGame();
                     run(players[j]);
-                    score += fitness();
+                    score += Main.g.game.score;
+                    if (isRipple()) {
+                        count++;
+                    }
+                }
+                if (count >= NUM_TRIAL-1) {
+                    score = 0;
                 }
                 scores[j] = score/NUM_TRIAL;
 //                System.out.println(scores[j]);
+                System.out.print(scores[j] + " ");
                 max = Math.max(max, scores[j]);
             }
+            System.out.println();
             System.out.println(max);
             System.out.println(i);
             Integer[] indices = new Integer[PLAYER_NUM];
@@ -60,7 +69,32 @@ public class Evolution {
         }
     }
     public int fitness(){
+        return 0;
+    }
 
+    public boolean isRipple() {
+        int[][] grid = Main.g.game.grid;
+        if ((grid[0][2] == grid[1][1]) && (grid[0][2] == grid[2][0])) {
+            return true;
+        } else if ((grid[1][3] == grid[2][2]) && (grid[1][3] == grid[3][1])) {
+            return true;
+        } else if (((grid[0][3] == grid[1][2]) && (grid[0][3] == grid[2][1])) ||
+                ((grid[0][3] == grid[1][2]) && (grid[0][3] == grid[3][0])) ||
+                ((grid[0][3] == grid[2][1]) && (grid[0][3] == grid[3][0])) ||
+                ((grid[1][2] == grid[2][1]) && (grid[1][2] == grid[3][0]))) {
+            return true;
+        }
+//        if ((grid[0][1] == grid[1][2]) && (grid[0][1] == grid[2][3])) {
+//            return true;
+//        } else if ((grid[1][0] == grid[2][1]) && (grid[1][0] == grid[3][2])) {
+//            return true;
+//        } else if (((grid[0][0] == grid[1][1]) && (grid[0][0] == grid[2][2])) ||
+//                ((grid[0][0] == grid[1][1]) && (grid[0][0] == grid[3][3])) ||
+//                ((grid[0][0] == grid[2][2]) && (grid[0][0] == grid[3][3])) ||
+//                ((grid[1][1] == grid[2][2]) && (grid[1][1] == grid[3][3]))) {
+//            return true;
+//        }
+        return false;
     }
     public void run(Player p) {
         for (;;) {
